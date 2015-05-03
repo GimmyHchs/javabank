0a2ed335720b332637941c33fa21069361409641
g9308370 <g9308370@hotmail.com>
2015-05-03 13:59:08 +0800

    edit the bank/show.blade.php

--- resources/views/bank/show.blade.php
+++ resources/views/bank/show.blade.php
@@ -3,21 +3,23 @@
 @section('bankcontent')
 
      
-    	    	<div class="panel-heading">Bank Show Information</div>
+    	    	<div class="panel-heading">{{$bank->name}}</div>
     	    	<div class="panel-body">
 
     	    		<div class="page">
     	    			{!!Form::open(['method'=>'GET','url'=>'/bank/'.$bank->code.'/edit'])!!}
     	    			         <div class="content">
-    	    						<p>
-                         	  		{{$bank->address}}		
-                       				</p>
-    	    						{!!Form::submit('EditTTTTTTTTT',['class'=>'btn btn-primary'])!!}
+	    	    						<div class="form-gruop">
+	                         	  			<p class="text-muted">{!!nl2br($bank->introduction)!!}</p>
+	                       				</div>
+	                       				<div class="form-group">
+	    	    							{!!Form::submit('Edit',['class'=>'btn btn-primary'])!!}
+	    	    						</div>
     	    					</div>
 						{!!Form::close()!!}
 					</div>
 
-					
+
     	    	</div>
           
     	  
